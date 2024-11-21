9be4c93819316a9b28f4456b0ad3c1b90b410683
fran14cruz <fran14.ich@gmail.com>
2024-11-21 10:02:59 -0500

    Add background dark

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from "./components/Hero";
 
 function App() {
   return (<>
+    {/* <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> */}
+    <div className="fixed -z-10 min-h-screen w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
     <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
       <Navbar />
       <Tech />
